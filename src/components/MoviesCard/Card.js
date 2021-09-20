4a51ab3273010a6a6f59d175797e90189c6c34f8
aajusah98 <computer9860@gmail.com>
2021-09-20 09:26:56 +0530

    watch list added

--- src/components/MoviesCard/Card.js
+++ src/components/MoviesCard/Card.js
@@ -5,12 +5,42 @@ import { useHistory } from 'react-router';
 
 
 
-const Card = ({ id, title, image, rating }) => {
+const Card = ({ id, title, image, rating, remove }) => {
 
     const history = useHistory();
 
     function movieDetails() {
         history.push({ pathname: '/MoviesDetails', state: id });
+
+    }
+
+    const setMovieToWatchList = (dataToBeAdded) => {
+        const data = JSON.parse(localStorage?.getItem('addToWatchList')) || [];
+        if (data.length > 0) {
+            const filteredData = data.find(item => item?.id === dataToBeAdded?.id);
+            if (filteredData?.id !== dataToBeAdded?.id) {
+                return localStorage.setItem('addToWatchList', JSON.stringify([...data, filteredData]))
+            }
+            return;
+        }
+
+        return localStorage.setItem('addToWatchList', JSON.stringify([...data, { ...dataToBeAdded }]))
+
+
+
+
+    }
+
+    const removeFromWatchList = (dataToBeAdded) => {
+        const data = JSON.parse(localStorage?.getItem('addToWatchList')) || [];
+        if (data.length > 0) {
+            const filteredData = data.filter(item => item?.id !== dataToBeAdded?.id);
+
+            localStorage.setItem('addToWatchList', JSON.stringify([...filteredData]));;
+            return window.location.reload();
+
+        }
+
 
     }
 
@@ -25,8 +55,13 @@ const Card = ({ id, title, image, rating }) => {
                         <p className="rating">{rating}/10</p>
                     </div>
                     <div className="btn-wrap">
-                        <button className="btn">add List</button>
-                        <button className="btn">Add Watched</button>
+
+                        {(!remove) && <button className="btn" onClick={() => setMovieToWatchList({ id, title, image, rating, remove: true })}>Add To Watch List</button>
+                        }
+
+                        {remove && <button className="btn" onClick={() => removeFromWatchList({ id })}>Remove From Watch List</button>
+                        }
+                        <button className="btn">Mark As Watched</button>
                     </div>
                 </div>
             </div>
--- src/pages/Watch_List.js
+++ src/pages/Watch_List.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Card from '../components/MoviesCard/Card'
 
 const Contact = () => {
+    const data = JSON.parse(localStorage.getItem('addToWatchList'));
+
+
     return (
         <div className="sec__one">
-            <h1> Watch_List </h1>
+            {
+                data.map(item => <Card id={item.id} title={item.title} image={item.image} rating={item.rating} remove={item.remove} />)
+            }
         </div>
     )
 }
